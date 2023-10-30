41d603501f9fa0f8bbcef66a61202ddaed04ce4b
fazzyA <draj29209@gmail.com>
2023-10-30 21:01:11 +0500

    61 - 64 chapters

--- 51-55/script.js
+++ 51-55/script.js
@@ -1,20 +1,67 @@
 console.log("connected");
-console.log("connected");
-console.log("connected");
-console.log("connected");
-console.log("connected");
-console.log("connected");
-console.log("connected");
-var btn = document.getElementById("btn_click");
+// console.log("connected");
+// console.log("connected");
+// console.log("connected");
+// console.log("connected");
+// console.log("connected");
+// console.log("connected");
+// var btn = document.getElementById("btn_click");
 
-function clickPara(element) {
-  console.log("THIS SELECT", element.className);
-  btn.className += " neClass"; //myBtn neClass
-  btn.style.background = "red";
-  btn.style.border = "0px";
-}
+// function clickPara(element) {
+//   console.log("THIS SELECT", element.className);
+//   btn.className += " neClass"; //myBtn neClass
+//   btn.style.background = "red";
+//   btn.style.border = "0px";
+// }
 
-function changeImageClick(id, path) {
-  console.log("ID", id);
-  document.getElementById(id).src = path;
-}
+// function changeImageClick(path) {
+//   // var imageNode = document.getElementById('image')
+//   document.getElementById('image').src = path
+//   // console.log("ID", id);
+//   // document.getElementById(id).src = path;
+// }
+// function expandLoris() {
+//   var expandedParagraph = `Slow lorises are a group of several species of trepsirrhine primates which
+//   make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of
+//   distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several
+//   adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time.
+//   Slow lorises have a toxic bite, a rare trait among mammals.`;
+//   document.getElementById("slowLoris").innerHTML = expandedParagraph;
+// }
+
+// function appendList() {
+//   var listParent = document.getElementById("list");
+//   console.log("UL", listParent);
+//   var listItems = "<li>ITEM 2</li>";
+//   listParent.innerHTML += listItems;
+// }
+
+// function clickHide() {
+//   var node = document.getElementById("image");
+//   // node.className += " hidden";
+//   node.style.backgroundColor = "red"
+//   node.style.border = "1px solid red"
+//   console.log(node);
+// }
+
+// function getAddress(){
+//   var link = document.getElementById('mnylink')
+//   console.log(link.href);
+// }
+
+// function changeColor() {
+//   var targetDiv = document.getElementById("first-container"); //parent node
+//   var paras = targetDiv.getElementsByTagName("p"); //
+//   for (var i = 0; i < paras.length; i++) {
+//     console.log("i", { i, para: paras[i] });
+    
+//     if(i % 2 === 0){
+//       paras[i].innerHTML += "<button>EVEN</button>"
+//       paras[i].style.color = "green";
+//     }else{
+//       paras[i].innerHTML += "<button>okkok</button>"
+//       paras[i].style.color = "red";
+//     }
+//   }
+// }
+console.log("DOCUMENT", document);
